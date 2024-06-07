ee7c28f41716a60cc71770fdba8fb42422372406
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-06-07 17:10:45 +0200

    fix: mailcheck now working correctly

--- mail/js/script.js
+++ mail/js/script.js
@@ -7,6 +7,7 @@ console.log("Gli invitati alla festa sono",invitati.length);
 //Chiedi all’utente la sua email 
 myBtn.addEventListener("click",
     function() {
+        let checkTrovato = false;
         const mailInput = document.querySelector("input");
         mailInputValue = mailInput.value;
         console.log(mailInputValue);
@@ -15,17 +16,16 @@ myBtn.addEventListener("click",
         for (let i = 0; i < invitati.length; i++) {
             let InvitatoIesimo = invitati[i];
             if(mailInputValue === InvitatoIesimo) {
-                document.getElementById("checkMessage").innerHTML = "Sei stato invitato";
-            }else {
-                document.getElementById("checkMessage").innerHTML = "Non sei stato invitato";
+                checkTrovato = true;
+                console.log("sono invitato");//stampa un messaggio appropriato sull’esito del controllo.
             }
+        }
+
+        if (checkTrovato === true) {
+            document.getElementById("checkMessage").innerHTML = "Sei stato invitato";
+        } else {
+            document.getElementById("checkMessage").innerHTML = "Non sei stato invitato";
         }
     }
 )
 
-
-
-
-
-
-//stampa un messaggio appropriato sull’esito del controllo.
