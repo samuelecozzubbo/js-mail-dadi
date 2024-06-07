5a65d0ebe0fbbb76e67bb98894b8a95b2bd8a108
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-06-07 15:23:09 +0200

    array created and addEventListener for mail input

---
+++ mail/js/script.js
@@ -0,0 +1,22 @@
+const myBtn = document.querySelector("button");
+//Creazione array con invitati
+const invitati = ['samuele@mail.com', 'giulia@mail.com', 'luca@mail.com', 'martina@mail.com', 'alessandro@mail.com', 'elena@mail.com', 'francesco@mail.com', 'sara@mail.com', 'matteo@mail.com', 'chiara@mail.com'];
+
+console.log("Gli invitati alla festa sono",invitati.length);
+//Chiedi all’utente la sua email 
+myBtn.addEventListener("click",
+    function() {
+        const mailInput = document.querySelector("input");
+        mailInputValue = mailInput.value;
+        console.log(mailInputValue);
+        
+        //controlla che sia nella lista di chi può accedere
+    }
+)
+
+
+
+
+
+
+//stampa un messaggio appropriato sull’esito del controllo.
